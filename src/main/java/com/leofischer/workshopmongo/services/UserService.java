ec6fde3eca9fe5094c688bdf03179a5bfa303e9c
LeoFischer <leombfischer@gmail.com>
2026-01-08 22:05:57 -0300

    Obtendo usuario por Id

--- src/main/java/com/leofischer/workshopmongo/services/UserService.java
+++ src/main/java/com/leofischer/workshopmongo/services/UserService.java
@@ -2,9 +2,11 @@ package com.leofischer.workshopmongo.services;
 
 import com.leofischer.workshopmongo.domain.User;
 import com.leofischer.workshopmongo.repository.UserRepository;
+import com.leofischer.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.List;
 
 @Service
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
