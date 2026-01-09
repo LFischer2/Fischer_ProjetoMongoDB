f8f2ff361c4e18bd159d872aec60f01b27f5117e
LeoFischer <leombfischer@gmail.com>
2026-01-09 13:04:30 -0300

    Obtendo um post por id

---
+++ src/main/java/com/leofischer/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.leofischer.workshopmongo.services;
+
+import com.leofischer.workshopmongo.domain.Post;
+import com.leofischer.workshopmongo.domain.User;
+import com.leofischer.workshopmongo.dto.UserDTO;
+import com.leofischer.workshopmongo.repository.PostRepository;
+import com.leofischer.workshopmongo.repository.UserRepository;
+import com.leofischer.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
